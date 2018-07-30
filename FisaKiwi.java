855a44cfac5afce974a82a97c2473a081faaca06
alexandrucozmanca <41379494+alexandrucozmanca@users.noreply.github.com>
2018-07-30 20:07:33 +0300

    Add files via upload

---
+++ FisaKiwi.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class FisaKiwi 
+{
+	public static void main(String[] args)
+	{
+		InputScreen app = new InputScreen();
+		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		app.setSize(633, 745);
+		app.setResizable(false);
+		app.setVisible(true);
+	}
+	
+
+}
